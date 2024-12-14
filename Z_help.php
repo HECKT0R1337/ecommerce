c46c398b5cbf6cb0a1bda75804c1b424942fccd2
HECKT0R1337 <msbmohamed8@gmail.com>
2024-12-14 10:07:37 +0200

    added some plugins and moved website from public folder to main folder

---
+++ Z_help.php
@@ -0,0 +1,25 @@
+<?php
+// git commands -->
+/*
+composer create-project laravel/laravel .
+
+git init									// will create .git folder but empty
+git add .									// will add all files except the ones at gitignore file
+git commit -m "first commit changes"
+git remote add origin https://github.com/heckt0rr1337/newfoldername.git
+git push -u origin master
+
+
+composer require maatwebsite/excel
+composer require barryvdh/laravel-dompdf
+composer require intervention/image
+composer require stripe/stripe-php
+
+*/
+
+
+
+
+
+
+
